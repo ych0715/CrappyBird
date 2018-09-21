a1b62f512b7fe1e16431b98c6481f30b130c27de
hschen2k <hschen2k@gmail.com>
2018-09-21 10:38:59 +0800

    Create crappybird_abi.js

---
+++ crappybird_abi.js
@@ -0,0 +1,30 @@
+var CrappyBirdABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "coins",
+				"type": "uint256"
+			}
+		],
+		"name": "setHighScore",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHighScore",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
